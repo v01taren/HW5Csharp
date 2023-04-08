3bc681213442666ffabe5a928428628084f9d06e
Valeriy Ziborov <ziboroffvalera@gmail.com>
2023-04-08 13:22:03 +0300

    Initial commit

---
+++ 34hw/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] startArray = GetArray(5, 100, 999);
+Console.WriteLine(String.Join(" ", startArray));
+Console.WriteLine($"Количество четных элементов в массиве = {EvenCount(startArray)}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int EvenCount(int[] array)
+{
+    int count = 0;
+    foreach (int i in array) 
+    {
+        if (i % 2 == 0) count++;
+    }
+    return count;
+}
+
+//(Foreach пометка для себя. Это для итерации по элементам массива или любой перечислимой коллекции.
+//Для одномерных массивов оператор foreach обрабатывает элементы в порядке возрастания индекса, начиная с индекса 0 и заканчивая индексом Length - 1:)
---
+++ 38hw/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] startArrayDouble = GetArrayDouble(6, 0, 20);
+Console.WriteLine(String.Join(" ", startArrayDouble));
+Console.WriteLine($"Разница = {GetDifference(startArrayDouble)}");
+
+double[] GetArrayDouble(int size, int minValue, int maxValue)
+{
+    double[] result = new double[size];
+
+    var randon = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = randon.Next(minValue, maxValue + 1) + randon.NextDouble(); //число с плавающей запятой
+    }
+    return result;
+}
+
+double GetDifference(double[] array)
+{
+    double min = array [0];
+    double max = array[0];
+    foreach (int i in array)
+    {
+        if (min > i) min = i;
+        if (max < i) max = i;
+    }
+    return max - min;
+}
